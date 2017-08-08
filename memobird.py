1181a431bc007e0a98e6c2a6d272c60e7f40062f
Paul "Rohja" LESELLIER <rohja@rohja.com>
2017-08-08 16:55:28 +0200

    Fix logger

--- memobird.py
+++ memobird.py
@@ -77,5 +77,5 @@ class MemobirdNotificationService(BaseNotificationService):
         c.add_text(SEPARATOR)
         #
         printId = self.memobird.print_multi_part_content(c)
-        logger.info("Print sheduled with id: {id}".format(id=printId))
+        _LOGGER.info("Print sheduled with id: {id}".format(id=printId))
         
